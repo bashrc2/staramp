6182d03f9d0cb0a28fd19633b55daefa87a75e49
Bob Mottram <bob@libreserver.org>
2023-09-06 13:48:29 +0100

    Extra option

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm *.png
-./staramp --dog 8 --dogthresh 145 --dogamp 80 --maxy 2100 -f images/stars1.png
+./staramp --dog 8 --dogthresh 145 --dogamp 80 --miny 0 --maxy 2100 -f images/stars1.png
